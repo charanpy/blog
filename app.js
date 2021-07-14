e75511508df89301fbbf6cc7767ce0b6407e9f84
charanpy <durgicharan81@example.com>
2021-07-14 21:41:03 +0530

    added add blog

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+require('dotenv').config();
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,7 +20,7 @@ app.use(express.static("public"));
 const mongoose=require("mongoose");
 
 const depreceate={ useNewUrlParser: true , useUnifiedTopology: true };
-mongoose.connect("mongodb://localhost:27017/journal",depreceate);
+mongoose.connect(process.env.MONGO,depreceate);
 //Schema
 const journalSchema={
   name:String,
@@ -91,6 +92,6 @@ app.get("/posts/:postId", function(req, res){
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
